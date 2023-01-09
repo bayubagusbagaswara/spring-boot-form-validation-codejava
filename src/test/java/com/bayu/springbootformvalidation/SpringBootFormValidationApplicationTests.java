f1d1bde1472ced31645659189ee3d060e294d62c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-01-09 14:07:44 +0700

    initial commit

---
+++ src/test/java/com/bayu/springbootformvalidation/SpringBootFormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.springbootformvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
